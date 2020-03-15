d46964599745211839a058df7ca409c982141c9c
Romina <rominar2008@gmail.com>
2020-03-15 00:43:18 -0300

    first commit

---
+++ src/test/java/ar/com/ada/sb/validation/handler/HandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.sb.validation.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
